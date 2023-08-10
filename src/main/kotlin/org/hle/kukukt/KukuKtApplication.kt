2691006f9c9528bd37dac262ba76e12dc79546b7
yinghanhung <hle.infdy@gmail.com>
2023-08-10 22:25:11 +0800

    initialize project

---
+++ src/main/kotlin/org/hle/kukukt/KukuKtApplication.kt
@@ -0,0 +1,11 @@
+package org.hle.kukukt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KukuKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<KukuKtApplication>(*args)
+}
---
+++ src/test/kotlin/org/hle/kukukt/KukuKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.hle.kukukt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KukuKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
