6f2db07d18f66024288f73e8761692f20d49246a
yinghanhung <hle.infdy@gmail.com>
2023-09-14 21:01:15 +0800

    Explicit datasource config beans

---
+++ src/main/kotlin/org/hle/kukukt/config/JdbcTemplateConfig.java
@@ -0,0 +1,16 @@
+package org.hle.kukukt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcTemplateConfig {
+
+    @Bean
+    public NamedParameterJdbcTemplate mariadbNamedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
